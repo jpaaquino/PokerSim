5c9033b95a90c9e31a5a48a2b85af273cce05b8c
Joao Paulo Aquino <jpaaquino@hotmail.com>
2018-10-12 20:57:14 -0300

    Initial Commit

---
+++ PokerSimTests/PokerSimTests.swift
@@ -0,0 +1,36 @@
+//
+//  PokerSimTests.swift
+//  PokerSimTests
+//
+//  Created by Joao Paulo Aquino on 12/10/18.
+//  Copyright © 2018 Joao Paulo Aquino. All rights reserved.
+//
+
+import XCTest
+@testable import PokerSim
+
+class PokerSimTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
